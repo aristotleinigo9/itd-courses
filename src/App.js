f3301df5fc71a0bbe17b90c77754bb6f24837bb3
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2024-01-06 13:44:41 +0300

    initial commit itd catalogue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {data} from './data.js';
 
 function App() {
+  const [search, setSearch] = useState('')
+  console.log(search)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className='text-center mt-4'>ITD Course Catalogue</h1>
+        <div>
+            <a href="https://google.com">
+               <button>
+                  Home
+               </button>
+          </a>
+        </div>
+        <Form>
+          <InputGroup className='my-3'>
+            <Form.Control 
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder='Search course name' />
+          </InputGroup>
+        </Form>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Area</th>
+              <th>Course name</th>
+              <th>Description</th>
+              <th>Learners</th>
+              <th>Training hours</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data
+              .filter ((item) => {
+                return search.toLowerCase() === ''
+                  ? item 
+                  : item.course_name.toLocaleLowerCase().includes(search);
+                })  
+              .map((item) => (
+                  <tr>
+                    <td>{item.area}</td>
+                    <td>{item.course_name}</td>
+                    <td>{item.description}</td>
+                    <td>{item.learners}</td>
+                    <td>{item.training_hours}</td>
+                  </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,141 @@
+export const data = [{
+    "id": 1,
+    "area": "Academic",
+    "course_name": "Basic Mathematics",
+    "description": "Topics include place value, rounding-off numbers, fractions, percents, decimals, unit conversion, interpreting tables and graphs, basic geomtry. Calculator is NOT allowed.",
+    "learners": "APNE, VCGNE, SST",
+    "training_hours": 100
+  }, {
+    "id": 2,
+    "area": "Academic",
+    "course_name": "Industrial Mathematics",
+    "description": "Topics include accuracy and precision of measurements, accuracy and precision rules when working with measurements, ratio and proportion, solving systems of linear equations, plane trigonometry. Scientific non-programmable calculator is allowed.",
+    "learners": "APNE, VCGNE, RFT, SST",
+    "training_hours": 60
+  }, {
+    "id": 3,
+    "area": "Academic",
+    "course_name": "English Functional 3",
+    "description": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
+    "learners": "APNE,VCGNE",
+    "training_hours": 175
+  }, {
+    "id": 4,
+    "area": "Academic",
+    "course_name": "English Writing Functional",
+    "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.",
+    "learners": "APNE, VCGNE, RFT, SST",
+    "training_hours": 311
+  }, {
+    "id": 5,
+    "area": "Academic",
+    "course_name": "Computer Literacy & Online Security Essentials",
+    "description": "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+    "learners": "APNE, VCGNE",
+    "training_hours": 30
+  }, {
+    "id": 6,
+    "area": "Job Skills",
+    "course_name": "Basic Operator Course",
+    "description": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+    "learners": "APNE, VCGNE",
+    "training_hours": 114
+  }, {
+    "id": 7,
+    "area": "Job Skills",
+    "course_name": "Refinery Basic Operator Course",
+    "description": "Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.",
+    "learners": "APNE, VCGNE, RFT",
+    "training_hours": 151
+  }, {
+    "id": 8,
+    "area": "Job Skills",
+    "course_name": "Maintenance Machinist",
+    "description": "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+    "learners": "APNE, VCGNE",
+    "training_hours": 169
+  }, {
+    "id": 9,
+    "area": "Safety",
+    "course_name": "Lock Out Tag Out",
+    "description": "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
+    "learners": "RFT, Contractors",
+    "training_hours": 84
+  }, {
+    "id": 10,
+    "area": "Safety",
+    "course_name": "Gas Testing",
+    "description": "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+    "learners": "RFT, Contractors",
+    "training_hours": 398
+  }, {
+    "id": 11,
+    "area": "Safety",
+    "course_name": "Safety Driving Program",
+    "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+    "learners": "APNE, VCGNE",
+    "training_hours": 10
+  }, {
+    "id": 12,
+    "area": "Driving & Heavy Equipment",
+    "course_name": "Rigger III",
+    "description": "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.",
+    "learners": "RFT, Contractors",
+    "training_hours": 146
+  }, {
+    "id": 13,
+    "area": "Driving & Heavy Equipment",
+    "course_name": "Crane Operator",
+    "description": "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+    "learners": "RFT, Contractors",
+    "training_hours": 182
+  }, {
+    "id": 14,
+    "area": "Academic",
+    "course_name": "Safety and Health",
+    "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+    "learners": "APNE, VCGNE",
+    "training_hours": 151
+  }, {
+    "id": 15,
+    "area": "Academic",
+    "course_name": "Cybersecurity Essentials",
+    "description": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
+    "learners": "APNE, VCGNE",
+    "training_hours": 271
+  }, {
+    "id": 16,
+    "area": "Job Skills",
+    "course_name": "Basic Safety Training",
+    "description": "Phasellus in felis. Donec semper sapien a libero. Nam dui.",
+    "learners": "APNE, VCGNE",
+    "training_hours": 69
+  }, {
+    "id": 17,
+    "area": "Driving & Heavy Equipment",
+    "course_name": "Driving Improvement Program",
+    "description": "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+    "learners": "RFT",
+    "training_hours": 357
+  }, {
+    "id": 18,
+    "area": "Safety",
+    "course_name": "Basic Life Support",
+    "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+    "learners": "APNE, VCGNE, RFT, Contractors",
+    "training_hours": 194
+  }, {
+    "id": 19,
+    "area": "Academic",
+    "course_name": "English Vocabulary for Firefighters",
+    "description": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+    "learners": "APNE, VCGNE",
+    "training_hours": 328
+  }, {
+    "id": 20,
+    "area": "Academic",
+    "course_name": "Transformology",
+    "description": "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+    "learners": "APNE, VCGNE",
+    "training_hours": 207
+  }]
